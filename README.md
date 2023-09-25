cc709b04f7ea468e1f3370aa91fb22c6310c27d6
David Bushell <david@dbushell.com>
2023-09-25 15:19:51 +0100

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# 🐢 Deno Turtle
+
+Deno Turtle is a Deno Deploy project that streams random binary data at incredibly slow speeds!
+
+## License
+
+MIT License
+
+* * *
+
+[MIT License](/LICENSE) | Copyright © 2023 [David Bushell](https://dbushell.com)
---
+++ mod.ts
@@ -0,0 +1,42 @@
+const TOTAL_BYTES = 1024 * 10;
+const CHUNK_SIZE = 64;
+const BPS = 64;
+
+const generate = function* (totalBytes: number, chunkSize: number) {
+  let remaining = totalBytes;
+  while (remaining > 0) {
+    const chunk = new Uint8Array(Math.min(remaining, chunkSize));
+    crypto.getRandomValues(chunk);
+    remaining -= chunk.length;
+    yield chunk;
+  }
+};
+
+Deno.serve((req: Request) => {
+  const stream = new ReadableStream({
+    async start(controller) {
+      const generator = generate(TOTAL_BYTES, CHUNK_SIZE);
+      const delay = (CHUNK_SIZE / BPS) * 1000;
+      while (true) {
+        const {value, done} = generator.next();
+        if (done) {
+          controller.close();
+          break;
+        }
+        controller.enqueue(value);
+        await new Promise((resolve) => setTimeout(resolve, delay));
+      }
+    }
+  });
+  const headers = new Headers();
+  headers.set('Access-Control-Allow-Origin', '*');
+  headers.set(
+    'Content-Disposition',
+    `attachment; filename="${TOTAL_BYTES}.bin"`
+  );
+  headers.set('Content-Type', 'application/octet-stream');
+  headers.set('Content-Length', String(TOTAL_BYTES));
+  return new Response(stream, {
+    headers
+  });
+});
